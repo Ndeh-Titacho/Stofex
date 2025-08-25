39923b4ba5f74f2080ec77f3f464bc914efeee62
Ndeh-Titacho <ndehjunior6@gmail.com>
2025-08-25 15:26:34 -0700

    adjustments

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div className='text-3xl font-bold'>404</div>
+      <p>Oops! Page not found</p>
+      <Link to="/" className='text-blue-400 text-underline'>Return to Home</Link>
+    </div>
   )
 }
 
